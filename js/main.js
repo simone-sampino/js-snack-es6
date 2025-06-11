adbc5d0a998fdab16e09a3ebf2894865f41ef242
simone-sampino <s.samp98@gmail.com>
2025-06-11 08:14:06 +0200

    Add racing bikes data structure for Snack 1 exercise

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 /* Snack 1
 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore. */
 
+/* 
+- Wilier 0 SLR – 6,5 kg
+- Cannondale SuperSix EVO Hi-MOD Disc Dura Ace – 8,0 kg
+- Trek Emonda 2020 – 6,81 kg
+- Trek Emonda SLR – 4,65 kg
+- SAVA Phantom 2.0 700C – 8,1 kg
+ */
+
+const racingBikes = [
+  {
+    bikeName: "Wilier 0 SLR",
+    weight: 6.5,
+  },
+  {
+    bikeName: "Cannondale SuperSix EVO Hi-MOD Disc Dura Ace",
+    weight: 8.0,
+  },
+  {
+    bikeName: "Trek Emonda 2020",
+    weight: 6.81,
+  },
+  {
+    bikeName: "Trek Emonda SLR",
+    weight: 4.65,
+  },
+  {
+    bikeName: "SAVA Phantom 2.0 700C",
+    weight: 8.1,
+  },
+];
+
 /* Snack2
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
