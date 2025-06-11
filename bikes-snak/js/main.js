3ef1f39ee1a4a3049e92f91b084d552809bc3b79
simone-sampino <s.samp98@gmail.com>
2025-06-11 08:30:40 +0200

    Add initial HTML and JavaScript teams snack exercise. Add cycle for lightest bike

--- bikes-snak/js/main.js
+++ bikes-snak/js/main.js
@@ -32,6 +32,21 @@ const racingBikes = [
   },
 ];
 
+// check in console
+console.log(racingBikes);
+
+// which one is the lightest?
+let lighterRacingBike = racingBikes[0];
+
+for (let i = 0; i < racingBikes.length; i++) {
+  const thisBike = racingBikes[i];
+  if (thisBike.weight < lighterRacingBike.weight) {
+    lighterRacingBike = thisBike;
+  }
+}
+
+console.log(lighterRacingBike);
+
 /* Snack2
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
